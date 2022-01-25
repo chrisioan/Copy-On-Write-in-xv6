e09c3f6b2885216e5f47a2a14a8c9f8baa8688a1
chrisioan <christos_00@hotmail.co.uk>
2022-01-25 20:21:12 +0200

    done

---
+++ kernel/spinlock.h
@@ -0,0 +1,21 @@
+// the kernel expects there to be RAM
+// for use by the kernel and user pages
+// from physical address 0x80000000 to PHYSTOP.
+#define KERNBASE 0x80000000L
+#define PHYSTOP (KERNBASE + 128*1024*1024)
+
+#define PGSIZE 4096 // bytes per page
+
+// Mutual exclusion lock.
+struct spinlock {
+  uint locked;       // Is the lock held?
+
+  // For debugging:
+  char *name;        // Name of lock.
+  struct cpu *cpu;   // The cpu holding the lock.
+};
+
+extern struct ref_spinlock {
+  struct spinlock lock;
+  int reference_count[PHYSTOP/PGSIZE];
+} ref_c;
